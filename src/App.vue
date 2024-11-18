e63a35ef0241d3dc7c6ff8719329319872db235a
simarone <samuel210@live.fr>
2024-11-18 17:14:21 +0100

    functions incrementer et decrementer added and worlking well

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <p>Compteur : {{ count }}</p>
   <Button v-on:click="incrementer">incrementer</Button>
+  <Button v-on:click="decrementer">decrementer</Button>
 </template>
 
 <script setup>
@@ -11,9 +12,15 @@ const count = ref(0)
 const incrementer = () => {
   count.value++;
 }
+const decrementer = () => {
+  count.value--;
+}
 </script>
 
+
 <style scoped>
+/* Scoped permet de limiter le CSS a cette page */
+
 p {
   color: red;
 }
