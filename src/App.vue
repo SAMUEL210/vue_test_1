8a63184d5fe249dcc94a017b46055a0f36b602dd
simarone <samuel210@live.fr>
2024-11-18 16:51:46 +0100

    Premier commit projet de booster en VueJs

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Bonjour</h1>
+  <p>On commence a travailler vue js comme il faut</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
