7c6e39f40714e88228d1d21adb66535a1de1436b
simarone <samuel210@live.fr>
2024-11-19 14:58:46 +0100

    Generating a list and deleting

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
   <p>Compteur : {{ count }}</p>
   <Button v-on:click="incrementer">incrementer</Button>
   <Button v-on:click="decrementer">decrementer</Button>
+  <ul>
+    <li v-for="movie in Movies">{{ movie }}<Button @click="deleteMovie(movie)">Supprimer</Button></li>
+  </ul>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
+const Movies = ref(["Spider Man 2", "John Wick 1", "Avengers 1", "Avengers End Game", "John Wick 3"])
 const count = ref(0)
 
 const incrementer = () => {
@@ -14,6 +18,9 @@ const incrementer = () => {
 }
 const decrementer = () => {
   count.value--;
+}
+const deleteMovie = (movie) => {
+  Movies.value = Movies.value.filter((m) => m != movie )
 }
 </script>
 
